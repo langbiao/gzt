5faf4093e4bcc62e33aa181a4ab2ba61e1e09a27
langbiao <724091684@qq.com>
2017-09-09 00:38:18 +0800

    Update IndexController.class.php

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -8,12 +8,38 @@ class IndexController extends BaseController{
         $this->display();
     }
     
-    //修改密码
+    //设置
     public function setting()
     {
         if (IS_POST) {
             $this->setePassword();
         }
         $this->display();
+    }
+    
+    //修改密码
+    public function setePassword()
+    {
+        $pw1 = I('password_new1');
+        $pw2 = I('password_new2');
+        
+        if(emppty($pw1) || empty($pw2)){
+            $this->error('密码不能为空');
+        }
+        if($pw1 ！= $pw1){
+            $this->error('两次密码输入不一致');
+        }
+        
+        $adminId = session('adminId');
+        
+        $flag = M('member', 'gztadmin_')->where(array('id'=>$adminId))->save(array('password'=>md5($pw1)));
+        
+        if($flag) {
+            $this->success('修改成功');
+        }else{
+            $this->success('修改失败，请重试');
+        }
+        
+        
     }
 }

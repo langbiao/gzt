649aae3f3373c92b0440956480680b7fc9d80b31
langbiao <724091684@qq.com>
2017-09-09 00:16:21 +0800

    Update IndexController.class.php

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -7,4 +7,13 @@ class IndexController extends BaseController{
     public function index(){
         $this->display();
     }
+    
+    //修改密码
+    public function setting()
+    {
+        if (IS_POST) {
+            $this->setePassword();
+        }
+        $this->display();
+    }
 }
